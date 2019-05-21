2e36bede0073e8a6b70354e166b5e9200dbea4b2
Yasuhiro Nitta <lp6m2014@gmail.com>
2019-05-21 17:27:59 +0900

    add generator code for HLS parameter declaration

--- python/myestimtaor.py
+++ python/myestimtaor.py
@@ -44,5 +44,50 @@ unscaled_weight_strarray = list(map(lambda val: "{:.8g}".format(val), unscaled_w
 
 code += "double unscaled_weight[{}] = {{\n".format(weight_num)  + "{}\n}};\n".format(",\n".join(map(lambda list: " " * 31 + ", ".join(list), np.array(unscaled_weight_strarray).reshape(-1, 10))))
 code += "double unscaled_bias = {:.8g};\n".format(unscaled_bias[0])
+
+bgr_weight = unscaled_weight[0:192].reshape(8,8,3)
+hsv_weight = unscaled_weight[192:384].reshape(8,8,3)
+hog_weight = unscaled_weight[384:].reshape(3,7,4,9);
+
+code += "const weight WeightData[WINDOW_BLOCKNUM_H][WINDOW_BLOCKNUM_W] = {\n"
+for by in range(3):
+    code += "{"
+    for bx in range(7):
+        code += "{"
+        for part in range(4):
+            code += "{"
+            for bin_index in range(9):
+                code += "{:.8g}".format(hog_weight[by][bx][part][bin_index])
+                if bin_index != 8:
+                	code += ", "
+            code += "}"
+            if part != 3:
+                code += ", "
+        code += "}"
+        if bx != 6:
+            code += ", \n"
+    code += "}"
+    if by != 2:
+        code += ", \n"
+code += "\n};"
+
+code += "\n"
+
+code += "const pixweight WeightData[4][8] = {\n"
+for y in range(4):
+    code += "{"
+    for x in range(8):
+        code += "{"
+        code += "{" + "{:.8g}".format(bgr_weight[2*y][x][0]) + ", " + "{:.8g}".format(bgr_weight[2*y][x][1]) + ", " +  "{:.8g}".format(bgr_weight[2*y][x][2]) + "}, "
+        code += "{" + "{:.8g}".format(bgr_weight[2*y][x][0]) + ", " + "{:.8g}".format(bgr_weight[2*y][x][1]) + ", " +  "{:.8g}".format(bgr_weight[2*y][x][2]) + "}, "
+        code += "{" + "{:.8g}".format(hsv_weight[2*y+1][x][0]) + ", " + "{:.8g}".format(hsv_weight[2*y+1][x][1]) + ", " + "{:.8g}".format(hsv_weight[2*y+1][x][2]) + "}, "
+        code += "{" + "{:.8g}".format(hsv_weight[2*y+1][x][0]) + ", " + "{:.8g}".format(hsv_weight[2*y+1][x][1]) + ", " + "{:.8g}".format(hsv_weight[2*y+1][x][2]) + "}"
+        code += "}"
+        if(x != 7):
+            code += ", \n"
+    code += "}\n"
+    if y != 3:
+        code += ", \n"
+code += "\n};"
 with open('./output/weights.h', mode='w') as f:
     f.write(code)
